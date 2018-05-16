25c9aaad7e837509484537111e2c45820a833e77
Marcus Brinkmann <marcus.brinkmann@ruhr-uni-bochum.de>
2018-05-16 14:23:29 +0200

    Add Packet::create_or_throw.

--- lib/openpgp/packet.cpp
+++ lib/openpgp/packet.cpp
@@ -1,13 +1,43 @@
-// OpenPGP format
-// Copyright 2017 The NeoPG developers
+// OpenPGP packet (implementation)
+// Copyright 2017-2018 The NeoPG developers
 //
 // NeoPG is released under the Simplified BSD License (see license.txt)
 
 #include <neopg/packet.h>
 
+#include <neopg/marker_packet.h>
+#include <neopg/public_key_packet.h>
+#include <neopg/public_subkey_packet.h>
+#include <neopg/raw_packet.h>
+#include <neopg/signature_packet.h>
+#include <neopg/user_id_packet.h>
+
+#include <neopg/parser_input.h>
 #include <neopg/stream.h>
 
+#include <neopg/intern/cplusplus.h>
+
 using namespace NeoPG;
+
+std::unique_ptr<Packet> Packet::create_or_throw(PacketType type,
+                                                ParserInput& in) {
+  switch (type) {
+    case PacketType::Marker:
+      return MarkerPacket::create_or_throw(in);
+    case PacketType::UserId:
+      return UserIdPacket::create_or_throw(in);
+    case PacketType::PublicKey:
+      return PublicKeyPacket::create_or_throw(in);
+    case PacketType::PublicSubkey:
+      return PublicSubkeyPacket::create_or_throw(in);
+    case PacketType::Signature:
+      return SignaturePacket::create_or_throw(in);
+    default:
+      // Should we do this?
+      return NeoPG::make_unique<RawPacket>(
+          type, std::string(in.current(), in.size()));
+  }
+}
 
 void Packet::write(std::ostream& out,
                    packet_header_factory header_factory) const {
